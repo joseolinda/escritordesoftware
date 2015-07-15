de38bd4024b2a2c755adbb00b152fb2b81ab8664
Jefferson Uchoa Ponte <jefponte@unilab.edu.br>
2015-07-15 10:20:21 -0300

    Tentando fazer a versão feita em Abril de 2013 existir. Na epoca eu sabia nem o que era GIT.

--- workspace/escritordesoftware/programas/blog/forms/forminserirblog.php
+++
@@ -1,6 +0,0 @@
-<form action='inserir_blog.php ' method='post'>
-				<fieldset><legend>Formulário para adicionar blog</legend>
-				 <label for="titulo">titulo:</label><input type="text" name="titulo" id="titulo" /><br> <label for="corpo">corpo:</label><input type="text" name="corpo" id="corpo" /><br>
-				<input type="submit" value="Cadastrar">
-				</fieldset>
-				</form>
--- workspace/escritordesoftware/teste.php
+++ workspace/escritordesoftware/teste.php
@@ -1,39 +1,11 @@
 <?php
 
-/*
- $criar = fopen("arquivo.php", "w+");
-if($criar==false)
-	die("impossivel criar o arquivo");
-*/
-
-/*
- // diretório onde encontra-se o arquivo
-$filename = "meu_arquivo.php";
-
-// verifica se existe o arquivo
-if(file_exists($filename)){
-$script = file_get_contents($filename);
-} else {
-$script = "";
-}
-
-//Adciona um novo texto
-$script = "Meu texto!!!" . $script;
-
-//Escrevendo
-$file = @fopen($filename, "w+");
-
-@fwrite($file, stripslashes($script));
-@fclose($file);
-
-*/
-
 function __autoload($classe){
 
 
-	if(file_exists("class/escritor/{$classe}.class.php"))
+	if(file_exists("class/especificas/{$classe}.class.php"))
 	{
-		include_once "class/escritor/{$classe}.class.php";
+		include_once "class/especificas/{$classe}.class.php";
 
 	}
 
@@ -41,17 +13,98 @@ function __autoload($classe){
 }
 
 
+$software = new Software();
+$software->setNome('escritor_de_software3');
+$tabela = new Tabela();
+$tabela->setNome('site');
+$tabela->setAtributos('
+software,
+banco_de_dados, 
+paginas');
+$tabela->setSeparador(',');
+
+
+$softwareTb = new Tabela();
+$softwareTb->setNome('software');
+$softwareTb->setAtributos('nome, objetos');
+$softwareTb->setSeparador(',');
+
+
+$objeto = new Tabela();
+$objeto->setNome('copiador');
+$objeto->setAtributos('caminho_inicial, caminho_final');
+$objeto->setSeparador(',');
+
+$atributo = new Tabela();
+$atributo->setNome('atributo');
+$atributo->setAtributos('nome, tipo, tamanho, indice, auto_increment');
+$atributo->setSeparador(',');
+
+$metodos = new Tabela();
+$metodos->setNome('metodo');
+$metodos->setAtributos('assinatura, documentacao');
+$metodos->setSeparador(',');
+
+
+
+$bancodedados = new Tabela();
+$bancodedados->setNome('banco_de_dados');
+$bancodedados->setAtributos('nome, host, senha, tipo');
+$bancodedados->setSeparador(',');
+
+
+$pagina = new Tabela();
+$pagina->setNome('pagina');
+$pagina->setAtributos('nome, objeto, atividade, conteudo_anterior, conteudo_posterior');
+$pagina->setSeparador(',');
+
+$arquivo = new Tabela();
+$arquivo->setNome('arquivo');
+$arquivo->setAtributos('caminho, nome, conteudo');
+$arquivo->setSeparador(',');
+
+$instanciador_rapido = new Tabela();
+$instanciador_rapido->setNome('instanciador_rapido');
+$instanciador_rapido->setAtributos('nome_do_software,nome_do_objeto,string_de_atributos, string_de_tipos');
+$instanciador_rapido->setSeparador(',');
+
+
+
+
+$tabelas = array($tabela, $softwareTb,$objeto, $atributo, $metodos, $bancodedados, $pagina, $arquivo, $instanciador_rapido);
+
+
+
+$software->setTabelas($tabelas);
+
 $banco = new Banco();
-$banco->setAtributos('id, nome, telefone');
-$banco->setSeparador(',');
-$banco->setNome('agendatelefonica');
+$banco->setHost('host');
+$banco->setName('blog');
+$banco->setPass('cocacola@12');
+$banco->setPort('8080');
+$banco->setType('mysql');
+
+$software->setBanco($banco);
+
+
+
 
 $controlador = new Controlador();
-$controlador->setBanco($banco);
+$controlador->setSoftware($software);
 $controlador->geraSoftware();
 
 
+/*
 
+$banco = new Tabela();
+$banco->setAtributos('name, host, pass, type, port');
+$banco->setSeparador(',');
+$banco->setNome('banco');
 
+$controlador = new Controlador();
+$controlador->setTabela($banco);
+$controlador->geraSoftware();
+
+*/
 
 ?>
